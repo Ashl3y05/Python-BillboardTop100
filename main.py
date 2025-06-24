5e9fd90d98f6321fb54bbdc0de04a3490ec01666
Ashley <panelo.ashley@gmail.com>
2025-06-24 21:38:18 +0800

    Stable version - Input a date and will now add a playlist to your spotify with all of the top 100 songs on that date

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from playlist import Playlist
 
 playlist = Playlist()
-selected_date = "1993-04-05" # input("Billboard top 100 on which date? (YYYY-MM-DD): ")
+selected_date = input("Billboard top 100 on which date? (YYYY-MM-DD): ")
 
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -20,12 +20,9 @@ titles_data = site_data.select("li ul li h3")
 
 titles = [data.get_text().strip() for data in titles_data]
 
-print(titles)
-num = 1
-for item in titles:
-    print(f"-----Track# {num}-----")
-    playlist.search_song(song_name=item)
-    num += 1
 
+playlist.create_playlist(name=f"{selected_date} Billboard 100", description=f"Top 100 songs from {selected_date}")
 
+track_uri_list = [playlist.search_song(song_name=songs) for songs in titles]
 
+playlist.add_song_to_playlist(tracks = track_uri_list)
--- playlist.py
+++ playlist.py
@@ -8,8 +8,11 @@ class Playlist:
         self.spotify_client_id = os.environ["SPOTIFY_CLIENT_ID"]
         self.spotify_secret = os.environ["SPOTIFY_SECRET"]
         self.redirect_uri = "http://127.0.0.1:8888/callback"
-        self.scopes = "user-read-private user-read-email user-top-read"
+        self.scopes = "user-read-private user-read-email user-top-read playlist-modify-public"
         self.access_token = self.get_access_token()
+        self.spotify_user = self.get_user_id()
+        self.playlist_id = ""
+
 
     def get_access_token(self):
         sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
@@ -19,8 +22,20 @@ class Playlist:
             scope=self.scopes
         ))
         return sp
-    def create_playlist(self):
-        pass
+
+    def get_user_id(self):
+        user_info = self.access_token.current_user()
+        return user_info["id"]
+
+    def create_playlist(self, name, description="N/A"):
+        playlist_name = name
+        user = self.spotify_user
+        playlist_desc = description
+
+        result = self.access_token.user_playlist_create(user=user,name=name,public=True, collaborative=False,
+                                                        description=playlist_desc)
+        self.playlist_id = result["id"]
+        print(f"Playlist Created! ID: {self.playlist_id}")
 
     def search_song(self,song_name, **artist):
         track_name = song_name
@@ -37,5 +52,11 @@ class Playlist:
             print("Track:", track['name'])
             print("Artist:", track['artists'][0]['name'])
             print("Spotify URI:", track['uri'])
+            return track['uri']
         else:
             print("Track not found")
+            return None
+
+    def add_song_to_playlist(self,tracks):
+        result = self.access_token.playlist_add_items(playlist_id=self.playlist_id,items=tracks,position=None)
+        print(f"List is added to playlist")
